ced5a286259c711a9e599e3f9ab16b647d9767e2
Nirmal Limbu <nirmalmaswa@live.com>
2026-08-06 11:06:30 +0100

    Updated project message on homepage

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -6,13 +6,20 @@ export default function Home() {
 			<main className="flex flex-1 w-full max-w-3xl flex-col items-center justify-between py-32 px-16 bg-white dark:bg-black sm:items-start">
 				<div className="flex flex-col items-center gap-6 text-center sm:items-start sm:text-left">
 					<h1 className="max-w-xs text-3xl font-semibold leading-10 tracking-tight text-black dark:text-zinc-50">
-						PostGIS
+						Elastic Search{" "}
+						<span className="text-sm text-olive-600">
+							+ PostGIS + Leaflet + Mapbox
+						</span>
 					</h1>
 					<p className="max-w-md text-lg leading-8 text-zinc-600 dark:text-zinc-400">
-						Integrate spatial capabilities with PostGIS{" "}
+						Geospatial search and visualization using Elastic
+						Search, PostGIS, Leaflet, and Mapbox. This project
+						demonstrates how to integrate these technologies to
+						create a powerful geospatial search and mapping
+						application.
 					</p>
 					<a
-						href="https://github.com/nexspy/postgis-learn"
+						href="https://github.com/nexspy/elastic_search"
 						target="_blank"
 						rel="noopener noreferrer"
 						className="text-amber-700 hover:underline"
@@ -28,12 +35,9 @@ export default function Home() {
 							Leaflet Map
 						</Link>
 
-						<Link
-							href="/maps/mapbox"
-							className="px-4 py-2 mt-4 text-white bg-amber-500 rounded hover:bg-amber-600"
-						>
+						<p className="px-4 py-2 mt-4 text-amber-800 bg-amber-300 rounded cursor-not-allowed">
 							Mapbox Map
-						</Link>
+						</p>
 					</div>
 				</div>
 			</main>
