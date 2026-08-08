e120e6d949e8fb9bcfee5995143d96d393df60b1
Nirmal Limbu <nirmalmaswa@live.com>
2026-08-08 05:36:43 +0100

    Adjust style of close button

--- frontend/app/(pages)/maps/leaflet/components/PropertyModalView.tsx
+++ frontend/app/(pages)/maps/leaflet/components/PropertyModalView.tsx
@@ -10,7 +10,7 @@ export const PropertyModalView = () => {
 					Description: This is a beautiful property located in a prime
 					area.
 				</p>
-				<button className="mt-4 px-4 py-2 bg-blue-500 text-white rounded hover:bg-blue-600">
+				<button className="px-4 py-2 mt-4 text-white bg-amber-500 rounded hover:bg-amber-600 cursor-pointer">
 					Close
 				</button>
 			</div>
