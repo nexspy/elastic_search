4e7477253b8e203756e3a9d4f5bd1dfe844f962e
Nirmal Limbu <nirmalmaswa@live.com>
2026-08-10 07:07:22 +0100

    Elastic Search uses Lon, Lat pairing

--- backend/src/util/conversion.util.ts
+++ backend/src/util/conversion.util.ts
@@ -5,17 +5,26 @@ import type { PropertyInAreaItem } from "../types/property/Property.type.ts";
  * Convert British National Grid coordinates (easting, northing) to latitude and longitude.
  * @param easting
  * @param northing
+ * @param forElasticSearch If true, returns coordinates in [lon, lat] order for Elasticsearch; otherwise returns [lat, lon].
  * @returns
  */
 export const convertBritishToLatLon = (
 	easting: number,
 	northing: number,
+	forElasticSearch: boolean = false,
 ): [number, number] => {
 	const britishNationalGrid =
 		"+proj=tmerc +lat_0=49 +lon_0=-2 +k=0.9996012717 +x_0=400000 +y_0=-100000 +ellps=airy +datum=OSGB36 +units=m +no_defs";
 	const wgs84 = "+proj=longlat +datum=WGS84 +no_defs";
 
+	// Convert the coordinates using proj4
 	const [lon, lat] = proj4(britishNationalGrid, wgs84, [easting, northing]);
+
+	if (forElasticSearch) {
+		// Swap lat and lon for Elasticsearch
+		return [lon, lat];
+	}
+
 	return [lat, lon];
 };
 
