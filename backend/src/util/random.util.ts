fbb050ca0914563ddeacdbbe3c3747531aedb8e3
Nirmal Limbu <nirmalmaswa@live.com>
2026-08-17 19:41:06 +0100

    Added random property name generator

--- backend/src/util/random.util.ts
+++ backend/src/util/random.util.ts
@@ -4,3 +4,12 @@ export const generateRandomStrForId = (length: number = 16): string => {
 		.toString(36)
 		.substring(2, 2 + length);
 };
+
+// Generate random property name
+export const generateRandomPropertyName = (
+	length: number = 8,
+	withSpace: boolean = false,
+): string => {
+	const randomStr = generateRandomStrForId(length);
+	return withSpace ? `_${randomStr} ` : `_${randomStr}`;
+};
